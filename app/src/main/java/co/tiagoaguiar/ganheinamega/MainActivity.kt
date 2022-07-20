5627e8a122527e1210e05712515e00fd012f0fb1
ERBCosta <eltonr@gmail.com>
2022-07-20 16:17:55 -0300

    Projeto atualizado com refatoração do código e identação.

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -1,5 +1,7 @@
 package co.tiagoaguiar.ganheinamega
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +13,9 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,6 +28,18 @@ class MainActivity : AppCompatActivity() {
         val txtResult: TextView = findViewById(R.id.txt_result)
         //criação do botão que vai gerar o jogo
         val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        //banco de dados de preferencias
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val result = prefs.getString("result", null)
+
+        /*if (result != null){
+            txtResult.text = "Ultima aposta: $result"
+        }
+        */
+        result.let {
+            txtResult.text = "Ultima aposta: $result"
+        }
 
         // opção 1 para escutar eventos de touch precisamos adicionar no xml a tag onclink:
         //android:onClick="buttonClicked"
@@ -41,39 +58,54 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-
     private fun numberGenerator(text: String, txtResult: TextView) {
-        //validar quando o campo é vazio
-        if (text.isNotEmpty()) {
-
-            val qtd = text.toInt() //converter string para inteiro
-
-            //validar se o campo informado é entre 06 e 15
-            if (qtd >= 6 && qtd <= 15) {
-
-                val numbers = mutableSetOf<Int>()
-                val random = Random()
-
-                while(true){
-                    val number = random.nextInt(60) //gerar de 0 até 59
-                    numbers.add(number + 1)
-
-                    if (numbers.size == qtd) {
-                        break
-                    }
-                }
-
-                txtResult.text = numbers.joinToString(" - ")
-
-            } else {
-                Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
-            }
-
-        } else {
+        //validar quando o campo é não é vazio falha numero 1
+        if (text.isEmpty()) {
+            // vai falhar
             Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
 
+        val qtd = text.toInt() //converter string para inteiro
+
+        //aqui é a falha numero 2
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+
+        }
+
+        //aqui é o sucesso
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60) //gerar de 0 até 59
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
+
+        val editor = prefs.edit()
+        editor.putString("result", txtResult.text.toString())
+        editor.apply()
+
+        /*alternativa 2
+        prefs.edit().apply(){
+            putString("result",txtResult.text.toString())
+            apply()
+        }
+        */
+
+        /*editor.commit -> Salva de forma sincrona (bloqueia a interface
+        * e informa se teve sucesso ou não*/
+
+        /*editor.apply -> Salva de forma assincrona (não bloqueia a interface
+        * e não informa se teve sucesso ou não*/
 
     }
 
